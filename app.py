cc0377409a9d98658300020213cd4ed4db8450e6
Uros Dikic <uros.dikic@gmail.com>
2026-04-29 15:01:29 +0200

    Update 8

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def build_vector_store(_documents: tuple    ):
 
     # --- Chunking ---
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
+        chunk_size=500,
         chunk_overlap=30,
         separators=["\n\n", "\n", ". ", " ", ""],
     )
